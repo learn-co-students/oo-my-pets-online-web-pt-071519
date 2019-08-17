b8872280d8cb0b306919b823ba697583dcb68003
Thomas DiGregorio <digregoriothomas@yahoo.com>
2019-08-16 18:59:48 -0700

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
   # code goes here
+  attr_accessor :mood, :owner
+  attr_reader :name
+  @@all = []
+  def initialize(name, owner)
+    @owner = owner 
+    @name = name
+    @mood = "nervous"
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,80 @@
+require 'pry'
 class Owner
   # code goes here
+  attr_accessor :pets
+  attr_reader :species, :name 
+
+  @@owners = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @pets = {:dogs => [], :cats => []}
+    #@pets = pets
+    @@owners << self 
+  end
+  def cats
+    
+    Cat.all.select{|cat|cat.owner == self}
+     
+  end
+  def dogs
+    
+  Dog.all.select{|dog|dog.owner == self}
+   
+end
+
+  def say_species
+    "I am a #{@species}."
+  end
+  
+  def buy_cat(cat_name)
+    binding.pry
+    #@pets[:cats] << Cat.new(cat_name)
+    self.cats.each do |cat|
+      self.pets[:cats] << cat
+    end
+  end
+
+  def buy_dog(dog_name)
+    @pets[:dogs] << Dog.new(dog_name)
+  end
+
+  def walk_dogs
+    @pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    @pets.each do |type, name_array|
+      name_array.each do |pet|
+        pet.mood = "nervous"
+        #name_array.delete(pet)
+      end 
+    end
+    @pets = {}
+  end
+
+  def list_pets
+    "I have #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
+  end
+
+  def self.all
+    @@owners
+  end
+
+  def self.reset_all
+    @@owners.clear
+  end
+
+  def self. count
+    @@owners.length
+  end
 end
